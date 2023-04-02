72a3e552bde4fc1572db742e6f465a754fbd2d5e
AliSiddique <alisiddique10@hotmail.com>
2023-04-02 13:16:49 +0100

    Initial Commit

---
+++ marzi/marziApp.swift
@@ -0,0 +1,17 @@
+//
+//  marziApp.swift
+//  marzi
+//
+//  Created by Ali Siddique on 02/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct marziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
